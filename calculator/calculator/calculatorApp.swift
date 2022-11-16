3943814ed5d13da2f3c37693ddf37dd09835141c
ciroforchetta1 <113618355+ciroforchetta1@users.noreply.github.com>
2022-11-16 15:22:31 +0100

    creation project

---
+++ calculator/calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by ciro forchetta on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
